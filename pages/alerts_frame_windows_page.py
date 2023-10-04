a7941a2734d6851d52841d78109b84d775068c40
Lipatnikova <aerovaya@gmail.com>
2023-10-04 19:00:01 +0400

    Add Alerts Frame & Windows Page

---
+++ pages/alerts_frame_windows_page.py
@@ -0,0 +1,10 @@
+from locators.alerts_frame_windows_page_locators import AlertsFrameWindowsPageLocators as locators
+from pages.base_page import BasePage
+
+
+class AlertsFrameWindowsPage(BasePage):
+
+    class BrowserWindowsPage:
+
+        def test(self):
+            pass
---
+++ tests/test_alerts_frame_windows_page.py
@@ -0,0 +1,10 @@
+import pytest
+import time
+
+from pages.alerts_frame_windows_page import AlertsFrameWindowsPage
+
+
+class TestAlertsFrameWindowsPage:
+
+    class TestBrowserWindowsPage:
+        pass
