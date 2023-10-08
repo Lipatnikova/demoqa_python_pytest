e1d5a0ec4461a4d4868d892d8e3a3b9665f37953
Lipatnikova <aerovaya@gmail.com>
2023-10-08 14:45:05 +0400

    Add Widgets Page

---
+++ pages/widgets_page.py
@@ -0,0 +1,9 @@
+from pages.base_page import BasePage
+from locators.widgets_page_locators import AccordianLocators as Accordian
+
+
+class WidgetsPage:
+    class AccordianPage(BasePage):
+
+        def sample(self):
+            pass
---
+++ tests/test_widgets_page.py
@@ -0,0 +1,7 @@
+from data.data_urls import ACCORDIAN_URL
+
+
+class TestWidgetsPage:
+
+    class AccordianPage:
+        pass
